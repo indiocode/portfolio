df04d83337340ac662986ca1945a5fb44f19b4e9
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-24 22:46:51 -0300

    :ambulance: update navbar styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,7 +7,6 @@ export const HeaderContainer = styled.header`
 	top: 0;
 	left: 0;
 	right: 0;
-	background: ${({ theme }) => theme.background};
 	z-index: 10;
 	display: flex;
 	align-items: center;
@@ -15,23 +14,29 @@ export const HeaderContainer = styled.header`
 `;
 
 export const Navbar = styled.nav`
+	display: flex;
+	padding: 1rem 2rem;
+	background-color: rgba(23, 24, 25, 0.8);
+	box-shadow: rgb(34 37 38 / 2%) 0px 0px 0px 1px inset,
+		rgb(0 0 0 / 5%) 0px 4px 30px 0px;
+	border-radius: 100px;
+
 	ul {
 		display: flex;
+		align-items: center;
 		gap: 1rem;
 
 		li {
 			list-style: none;
-
 			a {
-				padding: 0.5rem 0;
 				text-decoration: none;
+				flex: 1;
 				font-weight: 700;
 				font-size: 1.125rem;
 				color: ${({ theme }) => theme.white};
 
 				&:hover {
 					color: ${({ theme }) => theme['green-02']};
-					font-weight: 400;
 					transition: color 0.2s, font-weight 0.2s;
 				}
 			}
